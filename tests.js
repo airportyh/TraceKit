2cf0c9a6fbbc1a524b7fd766e7060a0d4c3e5cd9
airportyh <airportyh@gmail.com>
2013-01-08 01:31:38 -0500

    Try fail with scripts.

--- tests.js
+++ tests.js
@@ -1,7 +1,7 @@
 
 console.log('TAP version 13')
 console.log('1..1')
-console.log('ok 1 should be ok')
+console.log('not ok 1 should be ok')
 console.log('# tests 1')
-console.log('# pass  1')
-console.log('# fail  0')
+console.log('# pass  0')
+console.log('# fail  1')
