d1388f6c467904e4b9195fc6e3aebc0760ee0bbc
airportyh <airportyh@gmail.com>
2013-01-08 02:32:50 -0500

    Try.

--- tests/testling-bootstrap.js
+++ tests/testling-bootstrap.js
@@ -2,6 +2,4 @@ var jasmineEnv = jasmine.getEnv()
 jasmineEnv.addReporter(new TAPReporter(function(msg){
   console.log(msg)
 }))
-window.onload = function() {
-  jasmineEnv.execute()
-}
+jasmineEnv.execute()
