0634595788d22aca9f1f30b0bb3c4305bb899a2f
airportyh <airportyh@gmail.com>
2013-01-08 13:57:56 -0500

    Trying scripting the dom.

--- tests/testling-bootstrap.js
+++ tests/testling-bootstrap.js
@@ -1,5 +1,20 @@
-var jasmineEnv = jasmine.getEnv()
-jasmineEnv.addReporter(new TAPReporter(function(msg){
-  console.log(msg)
-}))
-jasmineEnv.execute()
+var scripts = ["tests/testling-presetup.js",
+  "tests/jasmine.js",
+  "tests/jasmine-tap.js",
+  "tests/tests.js",
+  "tracekit.js",
+  "tests/testling-bootstrap.js"]
+
+for (var i = 0; i < scripts.length; i++){
+    var script = scripts[i]
+    document.write('<script src="https://raw.github.com/airportyh/TraceKit/master/' + 
+        script + '"></' + 'script>')
+}
+
+setTimeout(function(){
+    var jasmineEnv = jasmine.getEnv()
+    jasmineEnv.addReporter(new TAPReporter(function(msg){
+      console.log(msg)
+    }))
+    jasmineEnv.execute()
+}, 0)
